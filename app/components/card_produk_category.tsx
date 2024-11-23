18ac67dedac418499fae0315eb0f132b8355f292
KwikAndreas <138807093+KwikAndreas@users.noreply.github.com>
2024-11-23 07:33:01 +0700

    fix error 2

--- app/components/card_produk_category.tsx
+++ app/components/card_produk_category.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-interface category_props {
-  width: 120;
-  height: 120;
-  src: string;
-  alt: string;
-  title: string;
-}
+// interface category_props {
+//   width: 120;
+//   height: 120;
+//   src: string;
+//   alt: string;
+//   title: string;
+// }
 
 const card_produk_category = () => {
   return <div></div>;
